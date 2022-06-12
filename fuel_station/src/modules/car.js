e341345151887a1e634e741adba7efdd74321f9c
anamaksimova <19cherrytree91@gmail.com>
2022-06-13 02:57:21 +0300

    m4_l4

---
+++ fuel_station/src/modules/car.js
@@ -0,0 +1,37 @@
+export class Car {
+    constructor(brand, model, maxTank) {
+        this.brand = brand;
+        this.model = model;
+        this.maxTank = maxTank;
+        this.nowTank = Math.floor(Math.random() * maxTank);
+      }
+
+      getTitle(){
+          return `${this.brand} ${this.model}`;
+      }
+
+     get needPetrol() {
+        return this.maxTank - this.nowTank;
+      };
+
+      fillUp() {
+        this.nowTank = this.maxTank;
+        return this;
+      }
+}
+
+
+export class PassangerCar extends Car {
+    typeCar = 'passanger';
+    constructor(brand, model, maxTank, typeFuel = 'petrol'){
+        super(brand, model, maxTank);
+        this.typeFuel = typeFuel;
+    }
+}
+export class Truck extends Car {
+    typeCar = 'truck';
+    constructor(brand, model, maxTank, typeFuel = 'diesel'){
+        super(brand, model, maxTank);
+        this.typeFuel = typeFuel;
+    }
+}
