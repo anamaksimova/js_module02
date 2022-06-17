cb3a7e8d1fc8bd2afb66f64fb61ab3f4fa961b1f
anamaksimova <19cherrytree91@gmail.com>
2022-06-18 02:21:37 +0300

    m4_l6

--- m3_lesson10/script.js
+++ m3_lesson10/script.js
@@ -33,6 +33,7 @@ const cardBuild = () => {
 
         el('button', {className: 'form__button'}, 'CHECK OUT'),
 
+
       ],
   );
 
@@ -41,7 +42,9 @@ const cardBuild = () => {
           [el('p', {className: 'secure'}, 'Secure Checkout'),
             card,
             form,
-          ]),
+            el('h1', {className: 'valid_check'}),
+          ]), el('button', {className: 'form__button valid'}, 'Отправить'),
+
 
   );
 };
@@ -57,18 +60,76 @@ const inputDate = document.querySelector('.input__date');
 const cvvInput = document.querySelector('.input__cvv');
 const form = document.querySelector('.form');
 
-const cardNumberMask = new Inputmask('9999-9999-9999-9999');
+const cardNumberMask = new Inputmask('****-****-****-****');
 cardNumberMask.mask(inputNumber);
 const cardDateMask = new Inputmask('99/99');
 cardDateMask.mask(inputDate);
 
 const cardNameMask = new Inputmask('a{1,20} a{1,20}');
-cardNameMask.mask(inputHolder);
-const cvvMask = new Inputmask('999');
+// cardNameMask.mask(inputHolder);
+const cvvMask = new Inputmask('***');
 cvvMask.mask(cvvInput);
 
 form.addEventListener('input', (e) => {
   if (e.target === inputHolder) cardHolder.textContent = inputHolder.value;
   if (e.target === inputNumber) cardNumber.textContent = inputNumber.value;
   if (e.target === inputDate) cardDate.textContent = inputDate.value;
+});
+
+const btnValid = document.querySelector('.valid');
+
+const justValidate = new JustValidate('.form');
+justValidate
+    .addField('.input__holder', [
+      {rule: 'required',
+        errorMessage: 'Укажите владельца карты',
+      },
+      {
+        validator(value) {
+          console.log('value: ', value);
+          const reg = /[A-Z]{2,}[\s][A-Z]/g;
+          return reg.test(value);
+        },
+        errorMessage: 'Укажите фамилию и имя латинскими буквами',
+      },
+    ])
+    .addField('.input__number',
+        [
+          {rule: 'required',
+            errorMessage: 'Укажите номер карты'},
+          {
+            validator(value) {
+              const cardNum = inputNumber.inputmask.unmaskedvalue();
+              return !!(Number(cardNum) && cardNum.length === 16);
+            },
+            errorMessage: 'Номер карты не корректен',
+          },
+
+        ])
+    .addField('.input__cvv',
+        [
+          {rule: 'required',
+            errorMessage: 'Укажите CVV'},
+          {
+            validator(value) {
+              const cvvNum = cvvInput.inputmask.unmaskedvalue();
+              return !!(Number(cvvNum) && cvvNum.length === 3);
+            },
+            errorMessage: 'CVV не корректен',
+          },
+
+        ]);
+
+
+btnValid.addEventListener('click', () => {
+  const h1 = document.querySelector('.valid_check');
+
+  if (justValidate.isValid) {
+    h1.textContent = 'Данные валидны';
+  } else {
+    h1.textContent = 'Данные  не валидны';
+  }
+  setTimeout(() => {
+    h1.textContent = '';
+  }, 2000);
 });
---
+++ m3_lesson10/script.test.js
@@ -0,0 +1,45 @@
+// / <reference types="cypress" />
+import './script.js';
+
+describe('Валидация Card Holder ', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/');
+  });
+  it('одно слово', () => {
+    cy.get('.input__holder').type('ANNA');
+    cy.get('.form__button').click();
+    cy.get('.input__holder').should('have.class', 'just-validate-error-field');
+  });
+  it('кириллица', () => {
+    cy.get('.input__holder').type('АННА');
+    cy.get('.form__button').click();
+    cy.get('.input__holder').should('have.class', 'just-validate-error-field');
+  });
+  it('с цифрами', () => {
+    cy.get('.input__holder').type('ANNA5');
+    cy.get('.form__button').click();
+    cy.get('.input__holder').should('have.class', 'just-validate-error-field');
+  });
+});
+
+
+describe('Валидация Card Number ', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/');
+  });
+  it('латиница', () => {
+    cy.get('.input__number').type('ANNAANNAANNAANNA');
+    cy.get('.form__button').click();
+    cy.get('.input__number').should('have.class', 'just-validate-error-field');
+  });
+  it('кириллица', () => {
+    cy.get('.input__number').type('АННААННААННААННА');
+    cy.get('.form__button').click();
+    cy.get('.input__number').should('have.class', 'just-validate-error-field');
+  });
+  it('латиница', () => {
+    cy.get('.input__number').type('ANNA,NNAANNAANNA');
+    cy.get('.form__button').click();
+    cy.get('.input__number').should('have.class', 'just-validate-error-field');
+  });
+});
